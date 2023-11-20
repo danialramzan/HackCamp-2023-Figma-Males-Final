09b0d17d8520ad4d8661b932313f664eb6f65bc6
Nishant Molleti <nish2005@students.ubc.ca>
2023-11-19 17:44:28 -0800

    made the account activity

---
+++ app/src/main/java/com/example/hackcamp2023project/Account.java
@@ -0,0 +1,20 @@
+package com.example.hackcamp2023project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+public class Account extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
--- app/src/main/java/com/example/hackcamp2023project/MainActivity.java
+++ app/src/main/java/com/example/hackcamp2023project/MainActivity.java
@@ -21,14 +21,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> logs;
     private ArrayAdapter<String> logsAdapter;
-
     private ListView listView;
     private Button button;
-
     private Button navigationButton;
-
+    private Button accountButton;
     private SeekBar seekBar;
-
     private TextInputEditText text;
 
 
@@ -40,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         button = findViewById(R.id.button16);
         navigationButton = findViewById(R.id.navigationButton);
+        accountButton = findViewById(R.id.profileButton);
         seekBar = findViewById(R.id.seekBar);
         text = findViewById(R.id.textInputEditText);
 
@@ -58,27 +56,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Account.class);
+                startActivity(intent);
+            }
+        });
+
         logs = new ArrayList<>();
         logsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, logs);
         listView.setAdapter(logsAdapter);
-        setUpListViewListener();
+        // setUpListViewListener();
 
     }
 
-    private void setUpListViewListener() {
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
-
-                logs.remove(position);
-                logsAdapter.notifyDataSetChanged();
-                return true;
-            }
-
-        });
-    }
+//    private void setUpListViewListener() {
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                Context context = getApplicationContext();
+//                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+//
+//                logs.remove(position);
+//                logsAdapter.notifyDataSetChanged();
+//                return true;
+//            }
+//
+//        });
+//    }
 
     private void addItem(View v) {
         String rating = Integer.toString(seekBar.getProgress());
